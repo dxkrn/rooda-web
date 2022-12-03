3a0c18aa3ae71447c03f7612818ccca5c2a8595c
dxkrnn <dxkrn@gmail.com>
2022-12-04 00:53:47 +0700

    Add: Sort and Search on StockMotor

--- stockMotor.php
+++ stockMotor.php
@@ -11,6 +11,23 @@ if (!isset(($_SESSION['username']))) {
 }
 $activeUser = $_SESSION['username'];
 
+//Inisialisasi nilai POST untuk sorting
+if ($_POST['sort_by'] == '') {
+  $sortBy = 'id_motor';
+  $sortType = 'ASC';
+  $_POST['sort_by'] = $sortBy;
+  $_POST['sort_type'] = $sortType;
+}
+
+//Inisialisasi nilai POST untuk searching
+if ($_POST['search_value'] == '') {
+  $searchValue = '';
+  $placeHolder = 'Search anything..';
+} else {
+  $searchValue = $_POST['search_value'];
+  $placeHolder = '';
+}
+
 //Menambah Motor Baru
 if (isset($_POST['submitTambahMotor'])) {
   $id_motor = getLastID($conn, 'tb_motor', 'id_motor', 'MT');
@@ -428,7 +445,46 @@ if (isset($_POST['submitHapus'])) {
               <!-- responsive table -->
 
               <div class="card">
-                <h3 class="card-header"></h3>
+                <h3 class="card-header">
+                  <div class="row g-2 d-flex justify-content-between">
+                    <!-- sort input -->
+                    <div class="col-md-6">
+                      <form method="POST">
+                        <div class="input-group">
+                          <select class="form-select" id="" aria-label="Example select with button addon" name="sort_by">
+                            <option selected value="<?= $_POST['sort_by'] ?>"><?= strtoupper(preg_replace("/_/", " ",  $_POST['sort_by'])) ?></option>
+                            <option value="id_motor">ID Motor</option>
+                            <option value="nama">Nama</option>
+                            <option value="nama_merk">Merk</option>
+                            <option value="nama_jenis_motor">Jenis</option>
+                            <option value="harga">Harga</option>
+                            <option value="stock">Stock</option>
+                            <option value="tipe_mesin">Tipe Mesin</option>
+                            <option value="volume_silinder">Volume Silinder</option>
+                            <option value="tipe_transmisi">Transmisi</option>
+                            <option value="kapasitas_bbm">Kapasitas BBM</option>
+                          </select>
+                          <select class="form-select" id="inputGroupSelect04" aria-label="Example select with button addon" name="sort_type">
+                            <option selected value="<?= $_POST['sort_type'] ?>"><?= $_POST['sort_type'] ?>ENDING</option>
+                            <option value="ASC">Ascending</option>
+                            <option value="DESC">Descending</option>
+                          </select>
+                          <button class="btn btn-primary" type="submit" name="submitSort">Sort</button>
+                        </div>
+                      </form>
+                    </div>
+
+                    <!-- search input -->
+                    <div class="col-md-4">
+                      <form method="POST">
+                        <div class="input-group">
+                          <input type="text" name="search_value" class="form-control" placeholder="<?= $placeHolder ?>" value="<?= $searchValue ?>" aria-describedby="button-addon2" />
+                          <button class="btn btn-primary" type="submit" id="button-addon2" name="submitSearch">Search</button>
+                        </div>
+                      </form>
+                    </div>
+                  </div>
+                </h3>
                 <div class="table-responsive text-nowrap">
                   <table class="table table-hover">
                     <thead>
@@ -448,9 +504,19 @@ if (isset($_POST['submitHapus'])) {
                       </tr>
                     </thead>
                     <tbody>
+
                       <?php
 
+                      if (isset($_POST['submitSort'])) {
+                        $sortBy = $_POST['sort_by'];
+                        $sortType = $_POST['sort_type'];
+                        header('refresh:0; url=stockMotor');
+                      }
 
+                      if (isset($_POST['submitSearch'])) {
+                        $searchValue = $_POST['search_value'];
+                        header('refresh:0; url=stockMotor');
+                      }
 
                       $ambil_data_stock = mysqli_query(
                         $conn,
@@ -463,7 +529,12 @@ if (isset($_POST['submitHapus'])) {
                           USING(id_jenis_motor)
                           JOIN tb_spesifikasi sp
                           USING(id_motor)
-                          ORDER BY mt.id_motor"
+                          WHERE nama LIKE '%$searchValue%' OR id_motor LIKE '%$searchValue%'
+                            OR nama_merk LIKE '%$searchValue%' OR nama_jenis_motor LIKE '%$searchValue%'
+                            OR harga LIKE '%$searchValue%' OR stock LIKE '%$searchValue%'
+                            OR tipe_mesin LIKE '%$searchValue%' OR volume_silinder LIKE '%$searchValue%'
+                            OR tipe_transmisi LIKE '%$searchValue%' OR kapasitas_bbm LIKE '%$searchValue%'
+                          ORDER BY $sortBy $sortType"
                       );
 
                       while ($data = mysqli_fetch_array($ambil_data_stock)) {
